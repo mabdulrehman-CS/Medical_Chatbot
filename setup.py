00168715f409b540d402237625134fde56e36ea3
mabdulrehman-CS <abdulrehmanmuhammad524@gmail.com>
2026-02-01 13:02:05 +0500

    Add setup.py file

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages, setup
+
+setup(
+    name = 'Medical Chatbot',
+    version= '0.0.0',
+    author= 'Muhammad Abdul Rehman',
+    author_email= 'mabdulrehman.cui@gmail.com',
+    packages= find_packages(),
+    install_requires = []
+
+)
